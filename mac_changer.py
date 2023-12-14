0d80a0ffbdee355afd7bed27936fe3c035c1ae63
Young-Jung <YoungJung365@gmail.com>
2023-12-14 03:10:10 -0500

    using conditional statements

--- mac_changer.py
+++ mac_changer.py
@@ -7,7 +7,13 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return options
+
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -18,6 +24,6 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
     subprocess.call("ifconfig", shell=True)
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
 
